1ca9f57e46a87ac830a2baf9742a446b36996213
BARASSIN Lenny <lbarassin.ira2026@esaip.org>
2023-12-21 17:44:00 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.volaille.volaille_front {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.volaille.volaille_front to javafx.fxml;
+    exports org.volaille.volaille_front;
+}
