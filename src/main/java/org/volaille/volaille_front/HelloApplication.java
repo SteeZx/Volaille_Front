94ea7cf17b0b662019244b1246e40a03b700c25c
SteeZx <lnybarassin@gmail.com>
2024-01-09 06:51:50 +0100

    Link du controller à la vue (dans le main-view.fxml)

--- src/main/java/org/volaille/volaille_front/HelloApplication.java
+++ src/main/java/org/volaille/volaille_front/HelloApplication.java
@@ -1,17 +1,24 @@
 package org.volaille.volaille_front;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 600);
+        fxmlLoader.setController(new HelloController());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/volaille/volaille_front/HelloController.java
+++ src/main/java/org/volaille/volaille_front/HelloController.java
@@ -1,25 +1,21 @@
 package org.volaille.volaille_front;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    ChoiceBox<String> volailletype;
+    ChoiceBox<String> choiceBox;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
-    void initialize() {
-        volailletype.getItems().add("Poulet");
-        volailletype.getItems().add("Canard");
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        choiceBox.getItems().addAll("Poulet", "Canard");
     }
 }
